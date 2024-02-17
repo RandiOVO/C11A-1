7949f8c042e30f174e79be73b291a324fbaf363e
lam-katie <lamkatie@mines.edu>
2024-02-16 19:06:44 -0700

    Added a CAT text label in the color cyan.

--- src/Cat.java
+++ src/Cat.java
@@ -44,6 +44,9 @@ public class Cat {
 		g2.setColor(Color.blue);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);
+		// cat label above the cat
+		g2.setColor(Color.cyan);
+		g2.drawString("CAT", catX+HEAD_DIMENSION-100, catY+HEAD_DIMENSION-150);
 	}
 }
